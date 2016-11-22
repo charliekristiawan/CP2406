22286ff94d73aaafa86770ab7c66de5e8cac9193
jc346395 <charliekristiawan@yahoo.com>
2016-11-22 13:18:43 +0800

    Week 1

---
+++ Week 1/MovieQuote.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jc346395 on 22/11/2016.
+ */
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("A boy's best friend is his mother.");
+    }
+}
---
+++ Week 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jc346395 on 22/11/2016.
+ */
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Night at the Museum, Alfred, 2006");
+    }
+}
